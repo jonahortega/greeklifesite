0469da8acdef2785f8d1f8eb29dfb194564c9b24
Jonah Ortega <jonahortega@Jonahs-MacBook-Pro.local>
2025-07-19 19:00:33 -0400

    Add basic test React app to debug white screen issue

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Greek Life Website</h1>
+        <p>Welcome to the future of Greek Life connectivity!</p>
+        <button style={{ padding: "10px 20px", fontSize: "16px", backgroundColor: "#667eea", color: "white", border: "none", borderRadius: "5px" }}>
+          Get Started
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
